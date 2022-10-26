d4bc1786def604e544d2bdee2f1641bbaa0141d5
Lewis Trowbridge <lewis.trowbridge@students.plymouth.ac.uk>
2022-10-26 15:40:41 +0100

    CFYP-41 - Retrieved DB username and password from env

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from pyaurn import importMeta
+import os
 
 metadata = importMeta()
 
-print(metadata)
+mongo_username = os.getenv("MONGO_DB_USER")
+mongo_password = os.getenv("MONGO_DB_PASS")
+
+if (mongo_username is None or mongo_password is None):
+    raise ValueError("MONGO_DB_USER or MONGO_DB_PASS not set")
